66c8ee0b94a40336ee22e77aeab5add799c693b4
Andres Rodriguez <anrocox@gmail.com>
2014-07-03 19:53:12 -0500

    How know what the largest and smallest item in the list?

---
+++ src/max_min_item_lists.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 2, 2014
+
+@author: anroco
+
+I have a list of numbers in python and I need to know what the largest and
+smallest item in the list?
+
+Tengo una lista de numeros en python y necesito saber cual es el mayor y el
+menor numero contenido en la lista?
+'''
+
+#create a list
+lista = [10, 12, 50, 1, 20]
+print (lista)
+
+#the max() function of built-in allows to know the highest value in the list.
+print(max(lista))
+
+#the min() function of built-in allows to know the lowest value in the list.
+print(min(lista))
